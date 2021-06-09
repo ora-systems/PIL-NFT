3412a398e97b20e421d60b6fa9b1531403a81e1a
bruce5f <brucer5f@gmail.com>
2021-06-09 13:13:37 -0700

    added HALO rotation

--- PIL.js
+++ PIL.js
@@ -12,15 +12,15 @@ var mousePressLoc = [];
 var lastYAngle = 0.0, lastXAngle = 0.0, yAngle = 0.0, xAngle = 0.0;
 
 var colorA = [], colorB = [];
-var tilt = 0.0, rotation = 0.0, speed = 0.0, wobble = 0.25, complexity = 0.5, colorRatio = 0.5, waveIntensity = 0.0;
+var tilt = 0.0, rotation = 0.0, rotationSpeed = 0.01, wobble = 0.25, complexity = 0.5, colorRatio = 0.5, waveIntensity = 0.5;
 
 var waveOffset = 0.0;
-var ringCount = 35, waveCount = 5;
+var ringCount = 75, waveCount = 3;
 function draw() {
     background(15);
     noFill();
     stroke(255);
-    strokeWeight(2);
+    strokeWeight(1);
     colorA.x = 255;
     colorA.y = 50;
     colorA.z = 100;
@@ -160,20 +160,37 @@ function draw() {
       vertex(x, y, z);
   }
   endShape();
+    
+  rotateX(-0.1);
+    rotateY(-time);
 
     
-    rotateX(xAngle);
-    rotateY(yAngle);
     /*HALO
+        Rings with two scales of perlin noise (wobble and complexity), wave effects, and color gradient
+        Parameters :
+        RING COUNT
+        COLOR GRADIENT (CENTER AND OUTER COLOR)
+        COLOR RATIO (balance between colors)
+        TILT (axis rotation)
+        ROTATION (radial rotation)
+        SPEED
+        WOBBLE (large scale perlin)
+        COMPLEXITY (small scale perlin)
+        WAVE COUNT
+        WAVE INTENSITY
+        WAVE COLOR
     */
+    strokeWeight(1);
     
+    rotateX(xAngle + tilt);
+    rotateY(yAngle);
     
   for (var i = 0; i < 1.0; i += 1.0/ringCount) {
     var iSmoothStep = 3*i*i - 2*i*i*i;
-    stroke(colorA.x*i + colorB.x*(1.0-i), colorA.y*i + colorB.y*(1.0-i), colorA.z*i + colorB.z*(1.0-i), 255);
-    if (floor((i-waveOffset) * ringCount) % (ringCount/waveCount) == 0) {
-      strokeWeight(4);
-    stroke(colorA.x*i + colorB.x*(1.0-i) + 255*(1-i), colorA.y*i + colorB.y*(1.0-i) + 255*(1-i), colorA.z*i + colorB.z*(1.0-i) + 255*(1-i), 255);
+    stroke(colorA.x*i + colorB.x*(1.0-i), colorA.y*i + colorB.y*(1.0-i), colorA.z*i + colorB.z*(1.0-i));
+    if (floor((i-waveOffset) * ringCount) % floor(ringCount/waveCount) == 0) {
+      strokeWeight(1 + 4*waveIntensity);
+    stroke(colorA.x*i + colorB.x*(1.0-i) + 255*(1-i)*waveIntensity, colorA.y*i + colorB.y*(1.0-i) + 255*(1-i)*waveIntensity, colorA.z*i + colorB.z*(1.0-i) + 255*(1-i)*waveIntensity);
     }
     beginShape();
     for (var t = 0; t < 1.05; t += 0.01) {
@@ -181,15 +198,18 @@ function draw() {
       var yNoiseA = complexity*i*500*(noise(3 + time + 6*cos(t*PI*2), 3 + time + 6*sin(t*PI*2), i*1-time)-0.5);
       var zNoiseA = complexity*i*300*(noise(3 + time + 5*cos(t*PI*2), 3 + time + 5*sin(t*PI*2), i*5-time)-0.5);
       var zNoiseB = wobble*i*2000*(noise(3 + time + 0.25*cos(t*PI*2), 3 + time + 0.25*sin(t*PI*2), i*1+time/5)-0.5);
-      vertex(10 + (radius*1.7 + iSmoothStep*150 + xNoiseA)*cos(t*PI*2), (radius*1.7 + iSmoothStep*150 + yNoiseA)*sin(t*PI*2), zNoiseA + zNoiseB);
+      vertex(10 + (radius*1.7 + iSmoothStep*150 + xNoiseA)*cos(t*PI*2 + rotation), (radius*1.7 + iSmoothStep*150 + yNoiseA)*sin(t*PI*2 + rotation), zNoiseA + zNoiseB);
     }
     endShape();
-    strokeWeight(2);
+    strokeWeight(1);
     
   }
     
-    waveOffset += 0.02;
+    waveOffset += 0.03;
     if (waveOffset > 1.0) waveOffset--;
+    
+    rotation += rotationSpeed;
+    if (rotation > PI*2) rotation -= PI*2;
     
     //Draw FX
     
